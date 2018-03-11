3781649c76d7cd6cfdc45350937e02c0888b1c34
Michael Job <budiman.job@gmail.com>
2018-03-11 16:01:13 +0100

    renamed contact and about to players and stats

--- matchit/src/pages/players/players.ts
+++ matchit/src/pages/players/players.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-players',
+  templateUrl: 'players.html'
 })
-export class ContactPage {
+export class PlayersPage {
 
   constructor(public navCtrl: NavController) {
 
