a8944a5b8e08eba7fe958b09d5cee614301c1afd
Michael Job <budiman.job@gmail.com>
2018-03-11 20:59:57 +0100

    tried to set camera function to avatar img

--- matchit/src/pages/players/players.ts
+++ matchit/src/pages/players/players.ts
@@ -8,24 +8,27 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 })
 export class PlayersPage {
 
+  private options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+
 
   constructor(public navCtrl: NavController, private camera: Camera) {
 
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
+  }
 
-    this.camera.getPicture(options).then((imageData) => {
+  takePicture() {
+    this.camera.getPicture(this.options).then((imageData) => {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64:
       let base64Image = 'data:image/jpeg;base64,' + imageData;
     }, (err) => {
       // Handle error
     });
-
   }
+
 
 }
